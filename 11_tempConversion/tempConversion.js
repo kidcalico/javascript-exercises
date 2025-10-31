fa1d3227a707111d94fa03ec7b1128d33d5c4c0f
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-31 15:45:09 -0700

    Complete temperature conversion exercise yay

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  const degreesC = ((degreesF - 32) * 5 / 9);
+  return (Math.round(degreesC * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  const degreesF = ((degreesC * 9 / 5) + 32);
+  return (Math.round(degreesF * 10) / 10);
 };
 
 // Do not edit below this line
