fa7636175114e775ff10647a46c6b002acd86a8c
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-30 18:38:42 -0700

    Add error message after looking at solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(string, num) {
-
+    // after looking at solution, add ERROR message if num < 0
+    if (num < 0) return 'ERROR';
     let finalString = '';
     for (i = 0; i < num; i++) {
         finalString += string;
