b849926a85b92bb7809713d9279017a197a7acbc
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-31 14:17:22 -0700

    Complete exercise eight *sweats*

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(arr, item) {
+//     const index = arr.indexOf(item);
+//     if (index > -1) {
+//         arr.splice(index, 1);
+//     }
+//     return arr;
+// };
 
+const removeFromArray = function(arr, ...items) {
+    if (arr.indexOf(items > -1)) {
+        const newArray = arr.filter(output => !items.includes(output))
+        return newArray;
+    }
+};
 // Do not edit below this line
 module.exports = removeFromArray;
