e9cdd6d8affcc2631b5e6df08112b22d2a96ed06
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-31 15:29:34 -0700

    Optimize code after viewing solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,14 +1,30 @@
+// First Attempt
+// 
+// const leapYears = function(year) {
+//     if (year % 100 === 0 && year % 400 === 0) {
+//         return true;
+//     } else if (year % 100 === 0) {
+//         return false;
+//     } else if (year % 4 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// Second attempt after looking at solution, this time I declared constants first
+// First time using a grouped `||` statement within an `&&` statement
+
 const leapYears = function(year) {
-    if (year % 100 === 0 && year % 400 === 0) {
-        return true;
-    } else if (year % 100 === 0) {
-        return false;
-    } else if (year % 4 === 0) {
+    const isDivByFour = year % 4 === 0;
+    const isDivByOneHun = year % 100 === 0;
+    const isDivByFourHun = year % 400 === 0;
+    
+    if (isDivByFour && (!isDivByOneHun || isDivByFourHun)) {
         return true;
     } else {
         return false;
     }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
