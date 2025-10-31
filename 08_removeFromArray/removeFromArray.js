d86a2628025b5e794b98389bf7492726063e6e8c
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-31 15:57:36 -0700

    Complete version based on basic solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,3 +1,6 @@
+// First try, couldn't figure out how to make this work for
+// subsequent tests
+// 
 // const removeFromArray = function(arr, item) {
 //     const index = arr.indexOf(item);
 //     if (index > -1) {
@@ -6,11 +9,28 @@
 //     return arr;
 // };
 
+// Second try, used google a bunch for this one, turns out
+// it's the more advanced way to solve this problem??
+
+// const removeFromArray = function(arr, ...items) {
+//     if (arr.indexOf(items > -1)) {
+//         const newArray = arr.filter(output => !items.includes(output))
+//         return newArray;
+//     }
+// };
+
+// One More time, this time to learn the more basic way to do it
+
 const removeFromArray = function(arr, ...items) {
-    if (arr.indexOf(items > -1)) {
-        const newArray = arr.filter(output => !items.includes(output))
-        return newArray;
-    }
-};
+    const newArray = [];
+    arr.forEach((item) => {
+        if(!items.includes(item)) {
+            newArray.push(item);
+        }
+    })  ;
+    return newArray;
+}
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
