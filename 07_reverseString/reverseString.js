151d9e3a3904441ee8061a3e842ca860b30026c3
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-10-30 18:43:52 -0700

    Complete first attempt

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split('');
+    let arrReversed = arr.reverse();
+    let stringReversed = arrReversed.join('');
+    return stringReversed;
 };
 
 // Do not edit below this line
